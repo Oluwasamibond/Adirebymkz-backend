0629735e836222ca633cd93fbf7902db2bce877b
Oluwasamibond <Oluwasamibond@gmail.com>
2025-03-10 11:13:10 +0100

    bckend

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://adirebymkz-frontend-finish.vercel.app",
+    origin: "https://adirebymkz-frontend-ite1.vercel.app",
     credentials: true,
   })
 );
